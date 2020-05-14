899700dae1a17a9a438fd50bf172573f2180e20e
Panagiotis Kapsalis <pkapsalis@epu.ntua.gr>
2020-05-14 18:19:56 +0300

    Some changes on settings

--- settings.py
+++ settings.py
@@ -98,7 +98,7 @@ JOB_NAMES = {
 }
 
 # AnalEyeZer Settings
-ANALEYEZER_HOST = os.environ.get('ANALEYEZER_HOST', '127.0.0.1')
+ANALEYEZER_HOST = os.environ.get('ANALEYEZER_HOST', 'qualichain.epu.ntua.gr')
 ANALEYEZER_PORT = os.environ.get('ANALEYEZER_PORT', 5000)
 QUERY_EXECUTOR_URL = 'http://{}:{}/ask/storage'.format(ANALEYEZER_HOST, ANALEYEZER_PORT)
 
