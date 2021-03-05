b09cd0133e087b99b1a7ff34705deeb0ead76b10
Panagiotis Kapsalis <pkapsalis@epu.ntua.gr>
2021-03-05 18:52:58 +0200

    changes on data handler

--- workers/data_handler.py
+++ workers/data_handler.py
@@ -100,8 +100,8 @@ class DataHandler(object):
                     user_id=user_id,
                     target_sector=data['targetSector'] if 'targetSector' in data.keys() else None,
                     description=data['description'] if 'description' in data.keys() else None,
-                    work_history=data['workHistory'] if 'workHistory' in data.keys() else [],
-                    education=data['education'] if 'education' in data.keys() else []
+                    work_history=data['workHistory'] if 'workHistory' in data.keys() else None,
+                    education=data['education'] if 'education' in data.keys() else None
                 )
                 self.session.add(new_cv)
                 self.session.commit()
