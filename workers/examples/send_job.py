d2ee7e328d6b3951bdda885fd47f1f5e1b4a7df4
Panagiotis Kapsalis <pkapsalis@epu.ntua.gr>
2021-03-18 15:15:37 +0200

    Some changes on error handling

--- workers/examples/send_job.py
+++ workers/examples/send_job.py
@@ -22,7 +22,7 @@ job = {
     "expectedSalary": None,
     "salaryCurrency": None,
     "uri": ":3995",
-    "id": "5991",
+    "id": "5997",
     "skillReq": [
         {
             "label": "PHP",
